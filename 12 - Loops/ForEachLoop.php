4e1e1d1f8babcbc36a475c2553d12c2a0625438c
Rasya Adrian <rasyaadrian1234@gmail.com>
2024-03-15 20:39:13 +0700

    initial commit

---
+++ 12 - Loops/ForEachLoop.php
@@ -0,0 +1,8 @@
+<?php
+
+// The foreach loop - Loops through a block of code for each element in an array or each property in an object.
+$colors = array("red", "green", "blue", "yellow");
+
+foreach ($colors as $x) {
+  echo "$x ";
+}
---
+++ 13 - Functions/Functions.php
@@ -0,0 +1,12 @@
+<?php
+
+function myMessage() {
+  echo "Hello world!";
+}
+
+myMessage();
+
+/* 
+Note: A function name must start with a letter or an underscore. 
+Function names are NOT case-sensitive.
+*/
